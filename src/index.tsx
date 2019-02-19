5b822612289a27e31719aee701c1e4f556eaa796
Panepo <whisper.ko@gmail.com>
2019-02-19 16:31:45 +0800

    Add plugin
    Add react google analytics and it's function

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReduxRoot from './ReduxRoot';
 import registerServiceWorker from './serviceWorker';
+import * as ReactGA from 'react-ga';
 
 const rootEl = document.getElementById('pilebunker');
 ReactDOM.render(<ReduxRoot />, rootEl);
+
+ReactGA.initialize('UA-106126363-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 if (module.hot) {
   module.hot.accept('./ReduxRoot', () => {
