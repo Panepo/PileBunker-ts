11a4d7c8774758d7b762c1c67797ea565addf96f
Panepo <whisper.ko@gmail.com>
2020-12-16 11:08:01 +0800

    update database
    update weapon database

--- src/constants/ConstParameters.ts
+++ src/constants/ConstParameters.ts
@@ -26,7 +26,7 @@ export interface WeaponSpecial {
 }
 
 export const wepIgnoreDef: WeaponSpecial[] = [
-  { value: 0.85, name: ['天之瓊矛', '鳳燐苦無[改]'] },
+  { value: 0.85, name: ['天之瓊矛', '鳳燐苦無[改]', '大黒潮見舞鶴[改]'] },
   {
     value: 0.9,
     name: [
@@ -34,7 +34,8 @@ export const wepIgnoreDef: WeaponSpecial[] = [
       '真・氏康の獅盾',
       'ヴァリス改',
       '真・元親の荒刃[改]',
-      '鳳燐苦無'
+      '鳳燐苦無',
+      '大黒潮見舞鶴'
     ]
   },
   { value: 0.92, name: ['ヴァリス', '幽閉の紅弓'] },
